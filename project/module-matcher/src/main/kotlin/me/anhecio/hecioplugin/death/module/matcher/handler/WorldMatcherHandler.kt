528f9c373204ea462a5b4310d9bbfef2fb9266fa
FROSTM0URNE <508679226@qq.com>
2025-06-01 01:38:41 +0800

    Update WorldMatcherHandler.kt

--- project/module-matcher/src/main/kotlin/me/anhecio/hecioplugin/death/module/matcher/handler/WorldMatcherHandler.kt
+++ project/module-matcher/src/main/kotlin/me/anhecio/hecioplugin/death/module/matcher/handler/WorldMatcherHandler.kt
@@ -9,16 +9,16 @@ import taboolib.library.configuration.ConfigurationSection
 
 object WorldMatcherHandler : MatcherHandler  {
     override val name: String = "World"
-    
+
     override fun match(event: PlayerDeathEvent, config: ConfigurationSection): Boolean {
         val requires = config.getConfigurationSection("requires") ?: return true
         val player = event.entity.player!!
-        // "in": 玩家名是 requires 中的任意一个
+        // "in": 世界名是 requires 中的任意一个
         requires.getStringList("in").takeIf { it.isNotEmpty() }?.let { nameList ->
             if (player.world.name !in nameList) return false
         }
 
-        // "out": 玩家名不是 requires 中的任意一个
+        // "out": 世界名不是 requires 中的任意一个
         requires.getStringList("out").takeIf { it.isNotEmpty() }?.let { nameList ->
             if (player.world.name in nameList) return false
         }
